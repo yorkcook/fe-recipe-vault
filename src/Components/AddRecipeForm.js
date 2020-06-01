0fe4182e620bc09ebaf357f2bbd00c88e08891d8
York Cook <yorkcook@Johnny-Castle.local>
2020-06-01 18:54:53 -0400

    WIP: working on clean refresh of add recipe

--- src/Components/AddRecipeForm.js
+++ src/Components/AddRecipeForm.js
@@ -1,196 +1,194 @@
-import React from 'react'
-import { addRecipe, getCategories } from '../Actions'
-import { connect } from 'react-redux'
-import styled from 'styled-components'
-import './AddRecipeForm.css'
+import React from "react";
+import { addRecipe, getCategories } from "../Actions";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import "./AddRecipeForm.css";
 
 const RecipeForm = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	margin: 30px;
-	height: 700px;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 30px;
+  height: 700px;
+`;
 
 const AddInput = styled.input`
-	width: 250px;
-	height: 30px;
-	border: none;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	background-color: white;
-	color: #0e1111;
-	font-size: 15px;
-	padding: 0 10px;
-	display: block;
-`
+  width: 250px;
+  height: 30px;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  color: #0e1111;
+  font-size: 15px;
+  padding: 0 10px;
+  display: block;
+`;
 
 const EachInput = styled.div`
-	width: 430px;
-	display: flex;
-	justify-content: space-between;
-	align-items: flex-start;
-	font-weight: bold;
-	font-family: 'Nunito', sans-serif;
-	font-size: 15px;
-	margin: 10px;
+  width: 430px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  font-weight: bold;
+  font-family: "Nunito", sans-serif;
+  font-size: 15px;
+  margin: 10px;
 
-	@media (max-width: 500px) {
-		flex-direction: column;
-		align-items: center;
-	}
-`
+  @media (max-width: 500px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
 
 const ButtonDiv = styled.div`
-	display: flex;
-	justify-content: space-between;
-	margin: 10px 0 0 0;
-`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px 0 0 0;
+`;
 
 const Button = styled.button`
-	margin: 10px;
+  margin: 10px;
 
-	width: 180px;
-	height: 40px;
-	background: #6d748c;
-	color: white;
-	font-size: 15px;
-	outline: none;
-	:hover {
-		background-color: white;
-		color: #6d748c;
-		border: 2px solid #6d748c;
-	}
-`
+  width: 180px;
+  height: 40px;
+  background: #6d748c;
+  color: white;
+  font-size: 15px;
+  outline: none;
+  :hover {
+    background-color: white;
+    color: #6d748c;
+    border: 2px solid #6d748c;
+  }
+`;
 
 class AddRecipeForm extends React.Component {
-	state = {
-		title: '',
-		source: '',
-		ingredient: '',
-		instructions: '',
-		category: ''
-	}
+  state = {
+    title: "",
+    source: "",
+    ingredient: "",
+    instructions: "",
+    category: "",
+  };
 
-	componentDidMount() {
-		this.props.getCategories()
-	}
+  componentDidMount() {
+    this.props.getCategories();
+  }
 
-	handleChange = (event) => {
-		this.setState({
-			[event.target.name]: event.target.value
-		})
-	}
-	createRecipe = (event) => {
-		console.log('addRecipe fields:', this.state)
-		event.preventDefault()
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  createRecipe = (event) => {
+    console.log("addRecipe fields:", this.state);
+    event.preventDefault();
 
-		this.props.addRecipe(this.state)
-		this.props.history.push('/recipes')
-	}
-	cancelAdd = (event) => {
-		event.preventDefault()
-		this.props.history.push('/recipes')
-	}
+    this.props.addRecipe(this.state);
+    this.props.history.push("/recipes");
+  };
+  cancelAdd = (event) => {
+    event.preventDefault();
+    this.props.history.push("/recipes");
+  };
 
-	render() {
-		return (
-			<div>
-				<div className='imageAdd'>
-					<RecipeForm>
-						<div className='recipeInput'>
-							<p className='title'>
-								Share Your Favorite Recipe...
-							</p>
-							<EachInput>
-								<div className='items'>
-									<p>TITLE :</p>
-								</div>
-								<AddInput
-									className='input'
-									onChange={this.handleChange}
-									placeholder='title'
-									value={this.state.title}
-									name='title'
-								/>
-							</EachInput>
+  render() {
+    return (
+      <div>
+        <div className="imageAdd">
+          <RecipeForm>
+            <div className="recipeInput">
+              <p className="title">Share Your Favorite Recipe...</p>
+              <EachInput>
+                <div className="items">
+                  <p>TITLE :</p>
+                </div>
+                <AddInput
+                  className="input"
+                  onChange={this.handleChange}
+                  placeholder="title"
+                  value={this.state.title}
+                  name="title"
+                />
+              </EachInput>
 
-							<EachInput>
-								<div className='items'>
-									<p>SOURCE :</p>
-								</div>
-								<AddInput
-									className='input'
-									onChange={this.handleChange}
-									placeholder='source'
-									value={this.state.source}
-									name='source'
-								/>
-							</EachInput>
-							<EachInput>
-								<div className='items'>
-									<p>INGREDIENTS :</p>
-								</div>
-								<textarea
-									className='instructionsInput'
-									onChange={this.handleChange}
-									placeholder='ingredients'
-									value={this.state.ingredient}
-									name='ingredient'></textarea>
-							</EachInput>
-							<EachInput>
-								<div className='items'>
-									<p>INSTRUCTIONS :</p>
-								</div>
-								<textarea
-									className='instructionsInput'
-									onChange={this.handleChange}
-									placeholder='instructions'
-									value={this.state.instructions}
-									name='instructions'></textarea>
-							</EachInput>
-							<EachInput>
-								<div className='items'>
-									<p>CATEGORY :</p>
-								</div>
-								<textarea
-									className='instructionsInput'
-									onChange={this.handleChange}
-									placeholder='instructions'
-									value={this.state.category}
-									name='category'></textarea>
-							</EachInput>
-							<ButtonDiv>
-								<Button
-									className='button'
-									type='submit'
-									onClick={(event) =>
-										this.createRecipe(event)
-									}>
-									{' '}
-									Add Recipe{' '}
-								</Button>
-								<Button
-									onClick={(event) => this.cancelAdd(event)}>
-									{' '}
-									Cancel
-								</Button>
-							</ButtonDiv>
-						</div>
-					</RecipeForm>
-				</div>
-			</div>
-		)
-	}
+              <EachInput>
+                <div className="items">
+                  <p>SOURCE :</p>
+                </div>
+                <AddInput
+                  className="input"
+                  onChange={this.handleChange}
+                  placeholder="source"
+                  value={this.state.source}
+                  name="source"
+                />
+              </EachInput>
+              <EachInput>
+                <div className="items">
+                  <p>INGREDIENTS :</p>
+                </div>
+                <textarea
+                  className="instructionsInput"
+                  onChange={this.handleChange}
+                  placeholder="ingredients"
+                  value={this.state.ingredient}
+                  name="ingredient"
+                ></textarea>
+              </EachInput>
+              <EachInput>
+                <div className="items">
+                  <p>INSTRUCTIONS :</p>
+                </div>
+                <textarea
+                  className="instructionsInput"
+                  onChange={this.handleChange}
+                  placeholder="instructions"
+                  value={this.state.instructions}
+                  name="instructions"
+                ></textarea>
+              </EachInput>
+              <EachInput>
+                <div className="items">
+                  <p>CATEGORY :</p>
+                </div>
+                <textarea
+                  className="instructionsInput"
+                  onChange={this.handleChange}
+                  placeholder="instructions"
+                  value={this.state.category}
+                  name="category"
+                ></textarea>
+              </EachInput>
+              <ButtonDiv>
+                <Button
+                  className="button"
+                  type="submit"
+                  onClick={(event) => this.createRecipe(event)}
+                >
+                  {" "}
+                  Add Recipe{" "}
+                </Button>
+                <Button onClick={(event) => this.cancelAdd(event)}>
+                  {" "}
+                  Cancel
+                </Button>
+              </ButtonDiv>
+            </div>
+          </RecipeForm>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-	categories: state.categories
-})
+  categories: state.categories,
+});
 
-export default connect(
-	mapStateToProps,
-	{ addRecipe, getCategories }
-)(AddRecipeForm)
+export default connect(mapStateToProps, { addRecipe, getCategories })(
+  AddRecipeForm
+);
---
+++ src/Utils/index.js
@@ -0,0 +1,212 @@
+import {
+	ADD_RECIPE_SUCCESS,
+	ADD_RECIPE_START,
+	ADD_RECIPE_FAILURE,
+	SEARCH_RECIPE,
+	EDIT_RECIPE_START,
+	EDIT_RECIPE_SUCCESS,
+	EDIT_RECIPE_FAILURE,
+	GET_RECIPES_START,
+	GET_RECIPES_SUCCESS,
+	GET_RECIPES_FAILURE,
+	GET_RECIPE_START,
+	GET_RECIPE_SUCCESS,
+	GET_RECIPE_FAILURE,
+	DELETE_RECIPE_SUCCESS,
+	DELETE_RECIPE_FAILURE,
+	DELETE_RECIPE_START,
+	GET_CATEGORIES_START,
+	GET_CATEGORIES_SUCCESS,
+	GET_CATEGORIES_FAILURE,
+	REGISTER_USER_FAILURE,
+	REGISTER_USER_START,
+	REGISTER_USER_SUCCESS,
+	LOGIN_USER_FAILURE,
+	LOGIN_USER_START,
+	LOGIN_USER_SUCCESS
+} from '../Actions'
+
+const initialState = {
+	isLoggedIn: false,
+	recipes: [],
+	recipesAccurate: false,
+	filteredRecipes: [],
+	categories: [],
+	addingRecipe: false,
+	fetchingRecipes: false,
+	fetchingRecipe: false,
+	updatingRecipe: false,
+	deletingRecipe: false,
+	fetchingCategories: false,
+	editingRecipe: false,
+	error: null,
+	registeringUser: false,
+	loggingIn: false,
+	recipe: {
+		title: '',
+		source: '',
+		description: '',
+		amount: '',
+		ingredient: '',
+		measurement: '',
+		instructions: '',
+		category: ''
+	}
+}
+
+function reducer(state = initialState, action) {
+	switch (action.type) {
+		case GET_RECIPES_START:
+			return {
+				...state,
+				fetchingRecipes: true
+			}
+		case GET_RECIPES_SUCCESS:
+			return {
+				...state,
+				fetchingRecipes: false,
+				recipes: action.payload,
+				filteredRecipes: action.payload,
+				recipesAccurate: true
+			}
+		case GET_RECIPES_FAILURE:
+			return {
+				...state,
+				fetchingRecipes: false,
+				recipesAccurate: false,
+				error: action.payload
+			}
+		case GET_RECIPE_START:
+			return {
+				...state,
+				fetchingRecipe: true
+			}
+		case GET_RECIPE_SUCCESS:
+			// console.log('either get the recipe or go hungry: ', action.payload)
+			return {
+				...state,
+				fetchingRecipe: false,
+				recipe: action.payload
+			}
+		case GET_RECIPE_FAILURE:
+			return {
+				...state,
+				fetchingRecipe: false,
+				error: action.payload
+			}
+		case DELETE_RECIPE_START:
+			return {
+				...state,
+				deletingRecipe: true
+			}
+		case DELETE_RECIPE_SUCCESS:
+			return {
+				...state,
+				deletingRecipe: false,
+				recipesAccurate: false
+			}
+		case DELETE_RECIPE_FAILURE:
+			return {
+				...state,
+				deletingRecipe: false,
+				error: action.payload
+			}
+		case ADD_RECIPE_SUCCESS:
+			return {
+				...state,
+				addingRecipe: true,
+				recipesAccurate: false
+			}
+		case ADD_RECIPE_START:
+			return {
+				...state,
+				addingRecipe: true
+			}
+		case ADD_RECIPE_FAILURE:
+			return {
+				...state,
+				addingRecipe: false,
+				error: action.payload
+			}
+		case SEARCH_RECIPE:
+			return {
+				...state,
+				filteredRecipes: action.payload
+			}
+		case REGISTER_USER_START:
+			return {
+				...state,
+				registeringUser: true
+			}
+		case REGISTER_USER_SUCCESS:
+			localStorage.setItem('token', action.payload.token)
+			return {
+				...state,
+				registeringUser: false,
+				isLoggedIn: true
+			}
+		case REGISTER_USER_FAILURE:
+			return {
+				...state,
+				registeringUser: false,
+				error: action.payload
+			}
+		case EDIT_RECIPE_START:
+			return {
+				...state,
+				editingRecipe: true
+			}
+		case EDIT_RECIPE_SUCCESS:
+			return {
+				...state,
+				editingRecipe: false,
+				recipesAccurate: false
+			}
+		case EDIT_RECIPE_FAILURE:
+			return {
+				...state,
+				editingRecipe: false
+			}
+		case GET_CATEGORIES_START:
+			//note: having specific categories isn't implemented, delete this if it doesn't break anything
+			return {
+				...state,
+				fetchingCategories: true
+			}
+		case GET_CATEGORIES_SUCCESS:
+			return {
+				...state,
+				fetchingCategories: false,
+				categories: action.payload
+			}
+		case GET_CATEGORIES_FAILURE:
+			return {
+				...state,
+				fetchingCategories: false,
+				error: action.payload
+			}
+		case LOGIN_USER_START:
+			return {
+				...state,
+				loggingIn: true
+			}
+		case LOGIN_USER_SUCCESS:
+			localStorage.setItem('token', action.payload.token)
+			localStorage.setItem('user_id', action.payload.user_id)
+			return {
+				...state,
+				loggingIn: false,
+				isLoggedIn: true
+			}
+		case LOGIN_USER_FAILURE:
+			return {
+				...state,
+				loggingIn: false
+			}
+
+		default:
+			return state
+	}
+}
+
+export default reducer
